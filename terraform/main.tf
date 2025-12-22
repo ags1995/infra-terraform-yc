373a39301123c5dec075393e4d73c1bdb3a76765
ags1995 <ahmadghazi95ii@gmail.com>
2025-12-22 22:17:33 +0300

    Complete Lab 5: Add Terraform config and fix Jenkins pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,72 @@
-data "yandex_vpc_network" "default" {
-  name = "default"
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "~> 0.129.0"
+    }
+  }
 }
-data "yandex_vpc_subnet" "default" {
-  name = "default-ru-central1-a"
+
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
+
+# Create a VPC network
+resource "yandex_vpc_network" "network" {
+  name = "lab5-network"
+}
+
+# Create a subnet in the specified zone
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "lab5-subnet"
+  zone           = var.zone
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+# Create VM instances
 resource "yandex_compute_instance" "vm" {
-  name        = "jenkins-vm"
-  platform_id = "standard-v3"
-  zone        = var.zone
+  count = var.vm_count
+  name = "lab5-vm-${count.index}"
+  
   resources {
     cores  = 2
     memory = 2
+    core_fraction = 20
   }
+
   boot_disk {
     initialize_params {
-      image_id = var.image_id
-      size     = 10
+      image_id = "fd87va5cc00gaq2f5qfb"  # Ubuntu 22.04
+      size = 10
     }
   }
+
   network_interface {
-    subnet_id = data.yandex_vpc_subnet.default.id
+    subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
   }
+
   metadata = {
-    ssh-keys = "${var.vm_user}:${file(var.ssh_public_key_path)}"
+    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
   }
+  
+  scheduling_policy {
+    preemptible = true  # Cheaper, can be terminated
+  }
+}
+
+# Output the public IP addresses
+output "vm_public_ips" {
+  value = yandex_compute_instance.vm[*].network_interface[0].nat_ip_address
+  description = "Public IP addresses of created VMs"
+}
+
+# Output SSH command examples
+output "ssh_commands" {
+  value = [for i, ip in yandex_compute_instance.vm[*].network_interface[0].nat_ip_address : "ssh ubuntu@${ip}"]
+  description = "SSH commands to connect to VMs"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,33 @@
-variable "service_account_key_file" {
-  description = "Path to Yandex Cloud Service Account key file"
-  type        = string
-  default     = "../credentials/sa-key.json"
-}
 variable "cloud_id" {
   description = "Yandex Cloud ID"
   type        = string
 }
+
 variable "folder_id" {
   description = "Yandex Cloud Folder ID"
   type        = string
 }
+
+variable "yc_token" {
+  description = "Yandex Cloud OAuth token"
+  type        = string
+  sensitive   = true
+}
+
 variable "zone" {
   description = "Yandex Cloud zone"
   type        = string
   default     = "ru-central1-a"
 }
-variable "image_id" {
-  description = "Yandex Cloud image ID"
-  type        = string
-  default     = "fd80ok8sil1fn2gqbm6h"
+
+variable "vm_count" {
+  description = "Number of VMs to create"
+  type        = number
+  default     = 1
 }
-variable "vm_user" {
-  description = "VM SSH user"
-  type        = string
-  default     = "ubuntu"
-}
+
 variable "ssh_public_key_path" {
   description = "Path to SSH public key"
   type        = string
-  default     = "~/.ssh/jenkins_deploy_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
