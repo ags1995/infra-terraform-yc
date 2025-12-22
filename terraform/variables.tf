3574faa5c8021642384f01e8bf2e6c08bdc62c61
ags1995 <ahmadghazi95ii@gmail.com>
2025-12-22 22:21:16 +0300

    Fix: Add default values to Terraform variables for Lab 5

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 variable "cloud_id" {
   description = "Yandex Cloud ID"
   type        = string
-  default     = "b1g34ocftjh5ugcdgth0"  # ← ADD DEFAULT VALUE
+  default     = "b1g34ocftjh5ugcdgth0"
 }
 
 variable "folder_id" {
   description = "Yandex Cloud Folder ID"
   type        = string
-  default     = "b1g1ei0qnal703m46gb2"  # ← ADD DEFAULT VALUE
+  default     = "b1g1ei0qnal703m46gb2"
 }
 
 variable "yc_token" {
   description = "Yandex Cloud OAuth token"
   type        = string
   sensitive   = true
-  default     = "y0__xCT2uu1CBjB3RMg8smB4BV11AWg3gGiVQIHmh6Yp2WqyGx4RQ"  # ← ADD DEFAULT
+  default     = "y0__xCT2uu1CBjB3RMg8smB4BV11AWg3gGiVQIHmh6Yp2WqyGx4RQ"
 }
 
 variable "zone" {
